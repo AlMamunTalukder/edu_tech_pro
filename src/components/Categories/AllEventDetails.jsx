d4e2942417f1c566118b8c1fd3f61d1e8b47a951
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-10-08 21:54:20 +0600

    Details page show

--- src/components/Categories/AllEventDetails.jsx
+++ src/components/Categories/AllEventDetails.jsx
@@ -1,12 +1,24 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import EventDetails from "./EventDetails";
+import { useEffect, useState } from "react";
 
 const AllEventDetails = () => {
-  const educations = useLoaderData();
+  const educations = useLoaderData(); //data load
+
+  const [education, setEducation] = useState({}); //data set
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const eventDetails = educations?.find(
+      (education) => education.id === Number(id)
+    );
+    setEducation(eventDetails);
+  }, [id, educations]);
 
   return (
     <div>
-      <EventDetails educations={educations}></EventDetails>
+      <EventDetails education={education}></EventDetails>
     </div>
   );
 };
